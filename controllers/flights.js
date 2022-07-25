c241032ec38bd9e8538612cad7e79f1a9101ee7c
jess <jessicamucklow@gmail.com>
2022-07-25 09:44:24 -0700

    update

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,14 +1,49 @@
 const Flight = require('../models/flight');
 
 module.exports = {
-    index,
-
+  index,
+  show,
+  new: newFlight,
+  create
 };
 
 function index(req, res) {
-    console.log('running')
-    Flight.find({}, function(err, flights) {
-      res.render('flights/index', { title: 'All Flights', flights });
-    });
-  }
+  console.log('running')
+  Flight.find({}, function (err, flights) {
+    console.log(flights)
+    res.render('flights/index', { title: 'All Flights', flights });
+  });
+}
 
+
+function show(req, res) {
+  Flight.findById(req.params.id)
+    .populate('')
+    .exec(function (err, flight) {
+      flight.find(
+        { _id: { $nin: flightNo } },
+        function (err, flight) {
+          res.render('flights/show', {
+            title: 'Details',
+          });
+        }
+      );
+    });
+}
+
+function newFlight(req, res) {
+  res.render('flights/new', { title: 'Add Flight' });
+}
+
+function create(req, res) {
+  // req.body.ticketsAvailable = !!req.body.ticketsAvailable;
+  console.log(req.body)
+  var flight = new Flight(req.body);
+  flight.save(function (err, flight) {
+    console.log(flight, "create")
+    // one way to handle errors
+    // if (err) return res.redirect('flights/new');
+
+    res.redirect(`/flights/${flight._id}`);
+  });
+}
--- models/flight.js
+++ models/flight.js
@@ -2,15 +2,26 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+
+const destinationSchema = new Schema({
+	airport: {
+		type: String,
+		enum: [ 'AUS', 'DFW', 'DEN', 'LAX', 'SAN' ],
+        required: true
+	},
+	arrival: Date
+});
+
 const flightSchema = new Schema({
     airline: {
         type: String, 
         enum: ['Alaska','American', 'Southwest', 'United'],
+        default: 'American',
         required: true
     }, 
     airport: {
         type: String, 
-        enum: ['PDX', 'DEN', 'DFW', 'LAX', 'BOS'],
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
         default: 'DEN',
         required: true
     },
@@ -22,15 +33,15 @@ const flightSchema = new Schema({
     },
     departs: {
         type: Date, 
-        default: function() {
-            const day = new Date().getDate();
-            const month = new Date().getMonth();
-            const nextYear =  new Date().getFullYear() + 1;
-            return new Date(nextYear, month, day)
-        }, 
-        required: true
+        // default: function() {
+        //     const day = new Date().getDate();
+        //     const month = new Date().getMonth();
+        //     const nextYear =  new Date().getFullYear() + 1;
+        //     return new Date(nextYear, month, day)
+        // }, 
+        // required: true
     }, 
-    // destination: [destinationSchema], 
+    destination: [destinationSchema]
     // ticket: [{type: Schema.Types.ObjectId, ref: 'Ticket'}],
 });
 
